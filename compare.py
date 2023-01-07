a88ca17e5477e821258102c92f183c84277d7ba7
Илья Соколов <sokolovila543@gmail.com>
2023-01-07 18:50:58 +0300

    212

--- compare.py
+++ compare.py
@@ -1,19 +1,33 @@
 import os
 import numpy as np
 
-# class File():
+class File():
 
+
+    '''
+    Класс File
+
+    Содержит метод Levenstien для сравения похожести 2 строк
+
+    '''
     
         
         
-#     def __init__(self,paths) -> None:
-#         self.paths=paths 
-        
-#         with open (self.paths ,"rt") as text:
-#             self.text=text
+    def __init__(self,path) -> None:
+        '''
+        path - пути до 2 файлов
+        передается кортежем или списком
+        '''
+        with open (path[0] ,"rt") as text:
+            self.__file1=text
+        with open (path[1] ,"rt") as text:
+            self.__file2=text
+    @classmethod #чтобы была возможность вызывать метод, не создавая объект класса
+    def Levenstein(self,file1, file2):
+        print("lev")
     
-#     def distance(self):
-#         pass
+
+
 def delete_element_from_list(lst,elem):
     '''
     удаляет все элементы со значением elem из списка lst
@@ -23,45 +37,8 @@ def delete_element_from_list(lst,elem):
             lst.remove(elem)
         except:
             break
-            
 
-def main():
     
-    i = "input.txt"#input() #файл с указанием путей для файлов
-    o = "scores.txt"#input() #файл с итоговыми значениями
-    paths=[]
-    
-
-    with open (i,'rt') as p: #считываем входной файл, в paths записываем кортежные пары названий сравниваемых файлов
-        paths=[tuple(text_file.split(" ")) for text_file in p]
-    
-
-    for s in paths:
-        s1,s2="",""
-        length1,length2=[],[]
-        with open (s[0],"rt") as string:
-            for text in string:
-             s1+=' '.join(text.split(" "))
-             length1.append(text)
-        with open (s[1],"rt") as string:
-           for text in string:
-             s2+=' '.join(text.split(" "))
-             length2.append(text)
-
-        delete_element_from_list(length1,'\n')
-        delete_element_from_list(length2,'\n')
-        print(length1,length2)
-        print(1-distance(str(s1),str(s2))/len(s1))
-        
-        
-        
-    
-    
-    with open (o,"w") as out:
-        pass
-
-
-def distance(s1, s2):
     '''
     Функция принимает 2 строки и считает
     расстояние Левенштейна между ними
@@ -81,9 +58,50 @@ def distance(s1, s2):
                 change += 1
             cur[j] = min(add, delete, change)
 
-    return cur[n]
+    return cur[n]      
+
+def main():
+    
+    i = "input.txt"#input() #файл с указанием путей для файлов
+    o = "scores.txt"#input() #файл с итоговыми значениями
+    
+    result=[]
+    with open (i,'rt') as p: #считываем входной файл, в paths записываем кортежные пары названий сравниваемых файлов
+        paths=[tuple(text_file.split(" ")) for text_file in p]
+    
+    for file in paths:
+        answer = File(file)
+        result.append(answer)
+
+    for s in paths:
+        s1,s2="",""
+        length1,length2=[],[]
+        with open (s[0],"rt") as string:
+            for text in string:
+             s1+=' '.join(text.split(" "))
+             length1.append(text)
+        with open (s[1],"rt") as string:
+           for text in string:
+             s2+=' '.join(text.split(" "))
+             length2.append(text)
+
+        delete_element_from_list(length1,'\n')
+        delete_element_from_list(length2,'\n')
+        print(length1,length2,sep='\n')
+        
+        
+        
+        
+    
+    
+    with open (o,"w") as out:
+        pass
+
+
+
+    
 
 if __name__ == "__main__":
-    main()
-    
+    #main()
+    File.Levenstein(1,1)
     
