790b3988854d336385d67f7083eb00e17cd31aca
Илья Соколов <sokolovila543@gmail.com>
2023-01-07 23:43:11 +0300

    n-commit

--- compare.py
+++ compare.py
@@ -7,7 +7,7 @@ class File():
     '''
     Класс File
 
-    Содержит метод Levenstien для сравения похожести 2 строк
+    Содержит метод Levenstein для сравения похожести 2 строк
 
     '''
     
@@ -18,13 +18,20 @@ class File():
         path - пути до 2 файлов
         передается кортежем или списком
         '''
-        with open (path[0] ,"rt") as text:
-            self.__file1=text
-        with open (path[1] ,"rt") as text:
-            self.__file2=text
-    @classmethod #чтобы была возможность вызывать метод, не создавая объект класса
-    def Levenstein(self,file1, file2):
-        print("lev")
+        self.__path=path
+        self.__file=[]
+        with open (path ,"rt") as text:
+            string = ''
+            for t in text:
+                self.__file.append(t)  
+        delete_element_from_list(self.__file,'\n') # удаляем лишние переносы строк
+    @property 
+    def file(self):
+        return self.__file
+    
+    @property
+    def path(self):
+        return self.__path
     
 
 
@@ -38,7 +45,7 @@ def delete_element_from_list(lst,elem):
         except:
             break
 
-    
+def Levenstein(s1, s2):      
     '''
     Функция принимает 2 строки и считает
     расстояние Левенштейна между ними
@@ -65,31 +72,20 @@ def main():
     i = "input.txt"#input() #файл с указанием путей для файлов
     o = "scores.txt"#input() #файл с итоговыми значениями
     
-    result=[]
+    paths = [] # массив с указанием путей к файлам
+    texts = [] # сравниваемые файлы
+
     with open (i,'rt') as p: #считываем входной файл, в paths записываем кортежные пары названий сравниваемых файлов
         paths=[tuple(text_file.split(" ")) for text_file in p]
     
     for file in paths:
-        answer = File(file)
-        result.append(answer)
+        arr = (File(file[0]),File(file[1])) 
+        #print(arr[0].get_file)
+    texts.append(arr)
 
-    for s in paths:
-        s1,s2="",""
-        length1,length2=[],[]
-        with open (s[0],"rt") as string:
-            for text in string:
-             s1+=' '.join(text.split(" "))
-             length1.append(text)
-        with open (s[1],"rt") as string:
-           for text in string:
-             s2+=' '.join(text.split(" "))
-             length2.append(text)
-
-        delete_element_from_list(length1,'\n')
-        delete_element_from_list(length2,'\n')
-        print(length1,length2,sep='\n')
-        
-        
+    
+    result=list( (len(arr[0].file[0]),len(arr[0].file[1])) for arr in texts)
+    print(result)
         
         
     
@@ -102,6 +98,6 @@ def main():
     
 
 if __name__ == "__main__":
-    #main()
-    File.Levenstein(1,1)
+    main()
+    
     
